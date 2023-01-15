c10c4e8f4c00eddf69003180fcd95a746ba21b69
Supriyo Rudra <117836404+rudrasro@users.noreply.github.com>
2023-01-15 12:10:18 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,14 +1,7 @@
 // complete the given function
 
 function palindrome(str){
-    let start = 0;
-    let end = str.length-1;
     str = str.toLowerCase();
-    while(start<end){
-        if(str.charAt(start) != str.charAt(end)){
-            return false;
-        }
-    }
-    return true;
+    return str === str.split('').reverse().join('');
 }
 module.exports = palindrome
